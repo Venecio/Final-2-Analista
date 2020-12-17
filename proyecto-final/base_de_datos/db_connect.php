bc91675deae146254da24ce159dcc2f5bc117fef
Cristian Pessio <48696162+Venecio@users.noreply.github.com>
2020-12-17 19:37:15 -0300

    Update db_connect.php

--- proyecto-final/base_de_datos/db_connect.php
+++ proyecto-final/base_de_datos/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){    
-        $conexion=new mysqli("localhost", "Venecio", "2277", "basephp");
+        $conexion=new mysqli("localhost", "", "", "basephp"); //saque user y password
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
         
